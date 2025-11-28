e32805ab930591485d21433df8c7d44b25c22784
Erzat Arzykulov IEMIT-25 <erzat.arzykulov@alatoo.edu.kg>
2025-11-28 16:15:45 +0600

    initial commit

---
+++ untitled/src/Main.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+        System.out.println("🤖 Добро пожаловать в игру 'Угадай число'!");
+        Game.startLoop();
+        System.out.println("👋 Спасибо за игру! До свидания.");
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
